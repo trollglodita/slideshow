bba1893001d00ddb5c3cd87e1074500bff7f0125
Trollglodita <1trollglodita@gmail.com>
2014-09-21 21:58:52 -0300

    Adding ImageSurface

--- src/SlideView.js
+++ src/SlideView.js
@@ -5,9 +5,15 @@ define(function(require, exports, module) {
     var Transform = require('famous/core/Transform');
     var RenderNode = require('famous/core/RenderNode');
 	var StateModifier = require('famous/modifiers/StateModifier');
+	var ImageSurface = require('famous/surfaces/ImageSurface');
+
+	var SlideData = require('data/SlideData');
 
     SlideView.DEFAULT_OPTIONS = {
-    	size: [400, 500]
+    	size: [400, 500],
+    	filmBorder: 15,
+    	photoBorder: 3,
+        photoUrl: SlideData.defaultImage
     };
 
     function SlideView(options) {
@@ -19,10 +25,12 @@ define(function(require, exports, module) {
 
        this.mainNode = this.add(this.rootModifier);
 
-       _creteBackground.call(this);
+       _createBackground.call(this);
+       _createFilm.call(this);
+       _createPhoto.call(this);
     }
 
-    function _creteBackground () {
+    function _createBackground () {
     	var background = new Surface({
                // undefined size will inherit size from parent modifier
             properties: {
@@ -32,6 +40,46 @@ define(function(require, exports, module) {
         });
 
         this.mainNode.add(background);
+    }
+
+    function _createFilm() {
+        this.options.filmSize = this.options.size[0] - 2 * this.options.filmBorder;
+
+        var film = new Surface({
+            size: [this.options.filmSize, this.options.filmSize],
+            properties: {
+                backgroundColor: '#222',
+                zIndex: 1
+            }
+        });
+
+        var filmModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            transform: Transform.translate(0, this.options.filmBorder, 1)
+        });
+
+        this.mainNode.add(filmModifier).add(film);
+    }
+
+    function _createPhoto() {
+        var photoSize = this.options.filmSize - 2 * this.options.photoBorder;
+
+        var photo = new ImageSurface({
+            size: [photoSize, photoSize],
+            content: this.options.photoUrl,
+            properties: {
+                zIndex: 2
+            }
+        });
+
+        this.photoModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            transform: Transform.translate(0, this.options.filmBorder + this.options.photoBorder, 2)
+        });
+
+        this.mainNode.add(this.photoModifier).add(photo);
     }
 
     SlideView.prototype = Object.create(View.prototype);
